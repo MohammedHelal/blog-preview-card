8de221b6a98f9bacb64fc77b278f8a2ee6e41259
Mohammed Omar Helal <mohd.omar.helal@gmail.com>
2024-07-28 14:56:09 +0800

    importing images in app.jsx for production

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import "./App.css";
+import illustration from "./assets/images/illustration-article.svg";
+import avatar from "./assets/images/image-avatar.webp";
 
 function App() {
   return (
     <div className="card">
-      <img
-        src="./src/assets/images/illustration-article.svg"
-        alt="card image"
-      />
+      <img src={illustration} alt="article illustraion" />
       <button>Learning</button>
       <p>Published 21 Dec 2023</p>
       <h1>HTML & CSS foundations</h1>
@@ -15,7 +14,7 @@ function App() {
         content, and presentation.
       </p>
       <div className="flex">
-        <img src="./src/assets/images/image-avatar.webp" alt="" />
+        <img src={avatar} alt="user profile image" />
         <h4>Greg Hooper</h4>
       </div>
     </div>
